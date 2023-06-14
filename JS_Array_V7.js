25492db35bf6a27aecb4873e22cc474cba79a11a
Alejandro Luces González <aluces@polytex.local>
2023-06-14 11:16:00 -0400

    Add_exercise_jschallenger_Array_V7

---
+++ JS_Array_V7.js
@@ -0,0 +1,17 @@
+// Write a function that takes an array (a) as argument. 
+// Return the number of elements in a.
+
+// Escriba una función que tome una matriz (a) como argumento. 
+// Devuelve el número de elementos en a.
+
+
+
+const myFunction =(a) =>{
+
+    return a.length
+
+}
+
+console.log(myFunction([1,2,2,4]));
+console.log(myFunction([9,9,9]));
+console.log(myFunction([4,3,2,1,0]));
