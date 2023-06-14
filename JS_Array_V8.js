ba9eb0e57bb1c655f9863b8b96efe3dd5b955197
Alejandro Luces González <aluces@polytex.local>
2023-06-14 17:25:16 -0400

    Add_change_exercise_jschallenger_Array_V8

--- JS_Array_V8.js
+++ JS_Array_V8.js
@@ -3,9 +3,34 @@
 
 // Escribe una función que tome una matriz de números como argumento. 
 // Devuelve el número de valores negativos en la matriz.
+/* 
+const myFunction =(a) =>{
+    let contador = 0;
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] < 0) {
+            contador++;
+        }
+    }
+    return contador;
 
-const myFuntion =(a) =>{
+      
+    }
 
- return
-  
+*/
+
+// Otra forma de resolver
+
+const myFunction =(a) =>{
+
+    return a.filter((el)=> el<0).length;
+
 }
+
+    
+
+console.log(myFunction([1,-2,2,-4]));//2
+console.log(myFunction([0,9,1]));//0
+console.log(myFunction([4,-3,2,1,0]));//1
+
+
+
