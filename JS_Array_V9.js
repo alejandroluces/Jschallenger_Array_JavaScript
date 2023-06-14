b2990811b95108a57ea22f7e015c24b7c0b80bfc
Alejandro Luces González <aluces@polytex.local>
2023-06-14 17:54:37 -0400

    Add_exercise_jschallenger_Array_V9

---
+++ JS_Array_V9.js
@@ -0,0 +1,18 @@
+// Ordenar una matriz de cadenas alfabéticamente
+
+// Write a function that takes an array of strings as argument. 
+// Sort the array elements alphabetically. Return the result.
+
+// Escriba una función que tome una matriz de cadenas como argumento. 
+// Ordene los elementos de la matriz alfabéticamente. Devuelve el resultado.
+
+
+const myFunction =(arr) =>{
+
+    return arr.sort()
+
+}
+
+console.log(myFunction(['b', 'c', 'd', 'a']));
+console.log(myFunction(['z', 'c', 'd', 'a', 'y', 'a', 'w']));
+
