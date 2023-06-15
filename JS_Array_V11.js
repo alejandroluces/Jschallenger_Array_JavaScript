e4b49afe59d96b22db8c90444dd52c6cac8789a5
Alejandro Luces González <aluces@polytex.local>
2023-06-15 17:27:56 -0400

    Add_exercise_jschallenger_Array_V11.

--- JS_Array_V11.js
+++ JS_Array_V11.js
@@ -12,11 +12,13 @@
 const myFunction = (a) => {
     return a.reduce((acc,cur) => acc+cur)
     
+    
 }
 
 console.log(myFunction([10,100,40]));
 console.log(myFunction([10,100,1000,1]));
 console.log(myFunction([-50,0,50,200]));
+
 
 
 
