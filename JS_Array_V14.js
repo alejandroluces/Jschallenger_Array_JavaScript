a3d8b39b835853f4af06798019355197516ceed7
Alejandro Luces González <aluces@polytex.local>
2023-06-15 23:30:57 -0400

    Add_exercise_jschallenger_Array_V14

---
+++ JS_Array_V14.js
@@ -0,0 +1,37 @@
+// Comprobar si todos los elementos de la matriz son iguales
+
+// Escribe una función que tome una matriz como argumento. Debería devolver verdadero si todos los elementos de la matriz son iguales. 
+// De lo contrario, debería devolver falso.
+
+/*
+Esta es otro forma de hacerlo
+
+
+function myFunction(arr) {
+    return arr.every((cur) => cur === arr[0]);
+    
+}
+
+
+
+*/
+
+function myFunction( arr ) {
+    return new Set(arr).size === 1
+  }
+
+console.log(myFunction([true, true, true, true]));
+console.log(myFunction(['test', 'test', 'test']));
+console.log(myFunction([1,1,1,2]));
+console.log(myFunction(['10',10,10,10]));
+
+
+
+
+
+
+
+
+
+
+
